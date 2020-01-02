7a013f0d49bb43a3da3efb08a79a5bdedea20455
Czasowski <czasowskii@gmail.com>
2020-01-02 20:04:28 +0200

    CLickable

--- app/src/main/java/com/example/recyclercoffee/CoffeeAdapter.java
+++ app/src/main/java/com/example/recyclercoffee/CoffeeAdapter.java
@@ -1,10 +1,11 @@
 package com.example.recyclercoffee;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,34 +15,46 @@ import java.util.List;
 
 public class CoffeeAdapter extends RecyclerView.Adapter<CoffeeAdapter.CoffeeViewHolder> {
 
-    public static class CoffeeViewHolder extends RecyclerView.ViewHolder {
+    private onCoffeeListener mOnCoffeeListener;
+
+    public static class CoffeeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener { //имплементим он клик
         public TextView nameTextView;
         public TextView descrTextView;
         public ImageView photoImg;
+        onCoffeeListener onCoffeeListener; //декларируем
 
-        public CoffeeViewHolder(View itemView) {
+        public CoffeeViewHolder(View itemView, onCoffeeListener onCoffeeListener) { //добавляем в констр
             super(itemView);
             nameTextView = itemView.findViewById(R.id.coffee_name);
             descrTextView = itemView.findViewById(R.id.coffee_description);
             photoImg = itemView.findViewById(R.id.coffee_photo);
+            this.onCoffeeListener = onCoffeeListener;
+            itemView.setOnClickListener(this); // крепим
+        }
+
+        @Override
+        public void onClick(View v) {
+            onCoffeeListener.onCoffeeClick(getAdapterPosition());//получаем позицию для метода в интерфейсе
         }
     }
 
     private List<Coffee> mCoffees;
-    CoffeeAdapter(List<Coffee> coffees) {
+    CoffeeAdapter(List<Coffee> coffees, onCoffeeListener onCoffeeListener) { //добавляем листенер
         mCoffees = coffees;
-        
+        mOnCoffeeListener = onCoffeeListener; //добавляем листенер
     }
     @NonNull
     @Override
-    public CoffeeViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+    public CoffeeViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) { //оздает новый объект ViewHolder всякий раз,
+        // когда RecyclerView нуждается в этом. Это тот момент, когда создаётся layout строки списка, передается объекту ViewHolder,
+        // и каждый дочерний view-компонент может быть найден и сохранен.
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.rows_of_coffee, viewGroup, false);
-        CoffeeViewHolder cvh = new CoffeeViewHolder(v);
+        CoffeeViewHolder cvh = new CoffeeViewHolder(v,mOnCoffeeListener); //добавляем листенер
         return cvh;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CoffeeViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CoffeeViewHolder holder, int position) { //принимает объект ViewHolder и устанавливает необходимые данные для соответствующей строки во view-компоненте.
         holder.nameTextView.setText(mCoffees.get(position).name);
         holder.descrTextView.setText(mCoffees.get(position).description);
         holder.photoImg.setImageResource(mCoffees.get(position).imageResId);
@@ -51,4 +64,9 @@ public class CoffeeAdapter extends RecyclerView.Adapter<CoffeeAdapter.CoffeeView
     public int getItemCount() {
         return mCoffees.size();
     }
+//для кликов 1е
+    public interface onCoffeeListener {
+        void onCoffeeClick(int position);
+    }
+
 }
--- app/src/main/java/com/example/recyclercoffee/DrinkActivity.java
+++ app/src/main/java/com/example/recyclercoffee/DrinkActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class DrinkActivity extends AppCompatActivity {
+public class DrinkActivity extends AppCompatActivity implements CoffeeAdapter.onCoffeeListener { //имплементим
 ArrayList<Coffee> coffees;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +19,20 @@ ArrayList<Coffee> coffees;
         setContentView(R.layout.activity_drink);
         RecyclerView rvCoffee = findViewById(R.id.rvCoffee);
         coffees = Coffee.initCoffee();
-        CoffeeAdapter adapter = new CoffeeAdapter(coffees);
+        CoffeeAdapter adapter = new CoffeeAdapter(coffees, this);
         rvCoffee.setAdapter(adapter);
-        rvCoffee.setLayoutManager(new LinearLayoutManager(this));
+        rvCoffee.setLayoutManager(new LinearLayoutManager(this)); //LayoutManager отвечает за позиционирование view-компонентов в RecyclerView,
+        // а также за определение того, когда следует переиспользовать view-компоненты, которые больше не видны пользователю.
+        }
+//для кликов метод
+    @Override
+    public void onCoffeeClick(int position) {
+        String messagetext = coffees.get(position).name + " пить будешь, бедолага?";
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messagetext);
+        Intent chosenIntent = Intent.createChooser(intent, "Кому плеснуть кофейка?");
+       startActivity(chosenIntent);
     }
 }
+
